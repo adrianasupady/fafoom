9da537c0621239ea1b9bb19078eb7cf27f7814bd
Adriana Supady <adriana.supady@gmail.com>
2015-03-26 14:18:25 +0100

    Change to RDKit

--- fafoom/__init__.py
+++ fafoom/__init__.py
@@ -1,8 +1,9 @@
 from structure import MoleculeDescription, Structure
 from angle import angle_measure, angle_set
-from check_geometry import is_geometry_valid
-from genetic_operations import selection, crossover_vec, mutation_tor_vec, mutation_cistrans_vec
+from genetic_operations import selection, crossover, mutation_tor,\
+ mutation_cistrans
 from new_molecule_parametrize import parametrize, template_sdf
 from pyaims import AimsObject
 from pyff import FFObject
+from pynwchem import NWChemObject
 from utilities import *
