04496ca002801b30016896d281a6214a0a063613
Adriana Supady <adriana.supady@gmail.com>
2015-03-26 14:41:29 +0100

    Docstrings, dependencies update

--- fafoom/pyff.py
+++ fafoom/pyff.py
@@ -1,78 +1,97 @@
-#    Copyright 2015 Adriana Supady 
+#    Copyright 2015 Adriana Supady
 #    adriana.supady@gmail.com
 #
 #    This file is part of fafoom.
 #
 #   Fafoom is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License as published by
+#   it under the terms of the GNU Lesser General Public License as published by
 #   the Free Software Foundation, either version 3 of the License, or
 #   (at your option) any later version.
 #
 #   Fafoom is distributed in the hope that it will be useful,
 #   but WITHOUT ANY WARRANTY; without even the implied warranty of
 #   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU General Public License for more details.
+#   GNU Lesser General Public License for more details.
 #
-#  You should have received a copy of the GNU General Public License
+#  You should have received a copy of the GNU Lesser General Public License
 #   along with fafoom.  If not, see <http://www.gnu.org/licenses/>.
 
-'''This module is a wrapper for OpenBabel force-field utility'''
-import openbabel
-import pybel
+'''Wrapper for RDKit force-field routines'''
 
-from utilities import xyz_to_xyz_readstring
+from rdkit import Chem
+from rdkit.Chem import AllChem
+from rdkit.Chem import ChemicalForceFields
+
 
 class FFObject():
-    '''This class allows for creating and handling force-field objects '''
+    """Create and handle force-field objects."""
     def __init__(self, parameter_file):
-        ''' __init__(parameter_file) -> None 
-        
-        Required parameters in the parameter_file:
-        force_field
-        
-        Optional parameters for the SteepestDescent method used for optimization:
-        steps (default=3000)
-        threshold (default=1e-10)
-        '''
-        
+        """Get the parameters from file.
+        Args(required):
+            parameter_file (str): name of the parameter file
+        Raises:
+            KeyError: if the force field in not defined
+            ValueError: if the force field is not 'uff' or 'mmff94'
+        Optional parameters for the minimization method used for optimization
+        that can be defined in the file:
+        steps (default=1000)
+        force_tol (default=1e-4)
+        energy_tol (default=1e-6)
+        """
         with open(parameter_file) as fin:
             parameter_dict = dict(line.strip().partition(' ')[::2] for line in fin)
+        if 'force_field' not in parameter_dict:
+            raise KeyError("The force field is not defined.")
         self.force_field = parameter_dict['force_field']
-        
-        s =[('steps', 3000), ('threshold', 1.0e-10)] 
+        if self.force_field not in ['uff', 'mmff94']:
+            raise ValueError("Unknown force field.")
+        s = [('steps', 1000), ('force_tol', 1.0e-4), ('energy_tol', 1.0e-6)]
         for k, v in s:
-            if k not in parameter_dict: parameter_dict[k]=v
-                
+            if k not in parameter_dict:
+                parameter_dict[k] = v
         self.steps = parameter_dict['steps']
-        self.threshold = parameter_dict['threshold']
-    
-    def run_ff(self, xyz_string):
-        ''' run_ff(xyz_string) -> None
-        
-        This function performs a force field optimization. '''
-        molecule = openbabel.OBMol()
-        xyz_to_xyz_readstring(molecule,xyz_string)
-        ff = openbabel.OBForceField.FindForceField(self.force_field)
-        ff.Setup(molecule)
-        ff.SteepestDescent(int(self.steps), float(self.threshold))
-        ff.UpdateCoordinates(molecule)
-        self.energy= float('{0:.4f}'.format(ff.Energy(molecule)))
-        pybelmol = pybel.Molecule(molecule)
-        self.xyz_string_optimized = pybelmol.write(format = 'xyz')
+        self.force_tol = parameter_dict['force_tol']
+        self.energy_tol = parameter_dict['energy_tol']
+
+    def run_ff(self, sdf_string):
+        """Perform the force field minimization.
+
+        Args:
+            sdf_string (str)
+        """
+        mol = Chem.MolFromMolBlock(sdf_string, removeHs=False)
+        if self.force_field == 'mmff94':
+            molprop = ChemicalForceFields.MMFFGetMoleculeProperties(mol)
+            ff = ChemicalForceFields.MMFFGetMoleculeForceField(mol, molprop)
+        elif self.force_field == 'uff':
+            ff = AllChem.UFFGetMoleculeForceField(mol)
+        ff.Minimize(int(self.steps),
+                    float(self.force_tol), float(self.energy_tol))
+        self.sdf_string_opt = Chem.MolToMolBlock(mol)
+        self.energy = float('{0:.4f}'.format(ff.CalcEnergy()))
 
     def get_energy(self):
-        '''  get_energy() -> energy
-        
-        This function retrieves the energy value or raises an exception if it is not present. '''
-        
-        if self.energy == None: 
-            raise Exception("The calculation wasn't performed yet. You need to run ff first.")
-        else: return self.energy
-    def get_xyz_string_opt(self):
-        ''' get_aims_string_opt() -> aims_string_opt
-        
-        This function retrieves the optimized string in the FHI-aims format and raises an exception if it is not present.'''
-        if self.xyz_string_optimized == None:
-            raise Exception("The calculation wasn't performed yet. You need to run ff first.")
-        else: return self.xyz_string_optimized
-    
+        """Get the energy of the molecule.
+
+        Returns:
+            energy (float)
+        Raises:
+            AttributeError: if energy hasn't been calculated yet
+        """
+        if not hasattr(self, 'energy'):
+            raise AttributeError("The calculation wasn't performed yet.")
+        else:
+            return self.energy
+
+    def get_sdf_string_opt(self):
+        """Get the optimized sdf string.
+
+        Returns:
+            optimized sdf string (str)
+        Raises:
+            AttributeError: if the optimization hasn't been performed yet
+        """
+        if not hasattr(self, 'sdf_string_opt'):
+            raise AttributeError("The calculation wasn't performed yet.")
+        else:
+            return self.sdf_string_opt
