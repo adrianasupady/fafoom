ebc416f00652f4d9fd184fc019de48e8d3d7d2da
adrianasupady <adriana.supady@gmail.com>
2015-07-15 17:22:49 +0200

    Added new module for assigning parameters

---
+++ fafoom/get_parameters.py
@@ -0,0 +1,131 @@
+#    Copyright 2015 Adriana Supady
+#
+#    This file is part of fafoom.
+#
+#   Fafoom is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Lesser General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   Fafoom is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#   along with fafoom.  If not, see <http://www.gnu.org/licenses/>.
+''' Communicate between the structure and the degrees of freedom.'''
+
+from rdkit import Chem
+from rdkit.Chem import AllChem
+from deg_of_freedom import Torsion, CisTrans, PyranoseRing
+from utilities import check_geo_sdf
+
+
+def get_atoms_and_bonds(smile):
+    """Build the molecule from SMILES and return the number of atoms and bonds.
+
+    Args(required):
+        smile (str): one-line representation of the molecule
+    Returns:
+        Number of atoms, number of bonds
+    """
+    mol = Chem.MolFromSmiles(smile)
+    if mol is None:
+        raise ValueError("The smile is invalid")
+    mol = Chem.AddHs(mol)
+    return mol.GetNumAtoms(), mol.GetNumBonds()
+
+
+def get_positions(type_of_deg, smile, **kwargs):
+    """Find the positions (tuples of atom indicies) of the degrees of freedom.
+
+    Args(required):
+        type_of_deg (str)
+        smile (str)
+        if cistrans should be optimized:
+            smart_cistrans
+    Args(optimal):
+        list_of_torsion (list)
+        smart_torsion (str)
+        filter_smart_torsion (str)
+        list_of_cistrans (list)
+        list_of_pyranosering (list)
+    Returns:
+        list of touples defining the positions of the degree of freedom
+    """
+
+    if type_of_deg == "torsion":
+        if 'list_of_torsion' in kwargs:
+            return Torsion.find(smile, positions=kwargs['list_of_torsion'])
+        else:
+            if 'smart_torsion' in kwargs:
+                if 'filter_smart_torsion' in kwargs:
+                    return Torsion.find(smile,
+                                        smart_torsion=kwargs['smart_torsion'],
+                                        filter_smart_torsion=
+                                        kwargs['filter_smart_torsion'])
+                else:
+                    return Torsion.find(smile,
+                                        smart_torsion=kwargs['smart_torsion'])
+            else:
+                return Torsion.find(smile)
+    if type_of_deg == "cistrans":
+        if 'list_of_cistrans' in kwargs:
+            return CisTrans.find(smile, positions=kwargs['list_of_cistrans'])
+        else:
+            return CisTrans.find(smile,
+                                 smart_cistrans=kwargs['smart_cistrans'])
+
+    if type_of_deg == "pyranosering":
+        if 'list_of_pyranosering' in kwargs:
+            return PyranoseRing.find(smile,
+                                     positions=kwargs['list_of_pyranosering'])
+        else:
+            return PyranoseRing.find(smile)
+
+
+def create_dof_object(type_of_deg, positions):
+    """Initialize the degree of freedom from the positions
+
+    Args:
+        type_of_deg (str)
+        positsion (list)
+    Returns:
+        degree of freedom object
+    """
+    if type_of_deg == "torsion":
+        return Torsion(positions)
+    if type_of_deg == "cistrans":
+        return CisTrans(positions)
+    if type_of_deg == "pyranosering":
+        return PyranoseRing(positions)
+
+
+def template_sdf(smile, distance_cutoff_1, distance_cutoff_2):
+    """Create a template sdf string and writes it to file.
+
+    Args(required):
+        smile (str): one-line representation of the molecule
+    Args(optional):
+        distance_cutoff_1 (float): min distance between non-bonded atoms [A]
+        distance_cutoff_2 (float): max distance between bonded atoms [A]
+    Returns:
+        sdf string
+    """
+    cnt = 0
+    sdf_check = True
+    while sdf_check:
+        mol = Chem.MolFromSmiles(smile)
+        mol = Chem.AddHs(mol)
+        AllChem.EmbedMolecule(mol)
+        AllChem.UFFOptimizeMolecule(mol)
+        Chem.SDWriter('mol.sdf').write(mol)
+        sdf_string = Chem.MolToMolBlock(mol)
+        check = check_geo_sdf(sdf_string, distance_cutoff_1, distance_cutoff_2)
+        if check:
+            sdf_check = False
+            Chem.SDWriter('mol.sdf').write(mol)
+        else:
+            cnt += 1
+    return sdf_string
